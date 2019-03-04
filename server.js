6ebe66518951cfb6e89b5a2850882752bb67870a
perci <Percivaltanner@gmail.com>
2019-03-04 13:19:33 -0800

    to spenc

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const methodOverride = require('method-override');
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
+
+app.get('/', loadHome)
 //express app creation
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -17,6 +19,12 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
+
+function loadHome(request, response) {
+    let SQL = `SELECT * FROM locations;`;
+    return client.query(SQL)
+        .then(results => response.render('index', {results:results.rows}))
+}
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
